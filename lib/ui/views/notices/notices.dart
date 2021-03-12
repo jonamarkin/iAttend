e5ff71e3d0d58ffa3d3c8c6b6844849da1fee92b
Ato <jonathan.markin@unionsg.com>
2021-03-12 17:53:52 +0000

    Request and notices files created

---
+++ lib/ui/views/notices/notices.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Notices extends StatefulWidget {
+  @override
+  _NoticesState createState() => _NoticesState();
+}
+
+class _NoticesState extends State<Notices> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
